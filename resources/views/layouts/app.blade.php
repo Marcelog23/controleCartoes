e334211007c037caf8dc0ccf0c5a8b71b37f614d
Marcelo <m23granzoto@gmail.com>
2019-04-06 18:49:01 -0300

    06/04/19 commit Marcelo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,35 +18,29 @@
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link " href="{{route('cartao.index')}}">Listagem</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('cartao.create')}}">Criar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('cartao.geraPdf')}}" target="_blank" >Gerar PDF</a>
-                        </li>
-                    </ul>
+                    @if (Route::has('login'))
+                        <div class="top-right links">
+                            @auth
+                                <ul class="nav navbar-nav">
+                                    <li class="nav-item ">
+                                        <a class="nav-link " href="{{route('cartao.index')}}">Listagem</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{route('cartao.create')}}">Criar</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{route('cartao.geraPdf')}}" target="_blank" >Gerar PDF</a>
+                                    </li>
+                                </ul>
+                                @else
+
+                             @endauth
+                        </div>
+                    @endif
+
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
